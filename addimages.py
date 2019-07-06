a65164c755b62edee48fa14d90184fb539b0dff8
Cam <camig.bustillo@hotmail.com>
2019-07-06 23:55:27 +0200

    Chan-chan's fantasy

--- addimages.py
+++ addimages.py
@@ -1,8 +1,9 @@
+#%%
 import os
 import shutil
 
-image_folder = "/home/cam/git/camgbus.github.io/images/miscellaneous/"
-page_to_change = "/home/cam/git/camgbus.github.io/miscellaneous.html"
+image_folder = "images/fantasy/"
+page_to_change = "fantasy.html"
 spacing = "\t"*5
 insertion_start = "<div class=\"gallery-bott\">"
 insertion_end = spacing+"<div class=\"clearfix\"> </div>"
@@ -10,11 +11,11 @@ insertion_end = spacing+"<div class=\"clearfix\"> </div>"
 gallery = ""
 
 for subdir, dirs, files in os.walk(image_folder):
-    for file in files:
+	for file in files:
 		gallery += ("\n"+spacing+"<div class=\"col-xs-4 col1 gallery-grid\">\n" +
-						spacing+"\t<a href=\"images/miscellaneous/"+file+"\" class=\"b-link-stripe b-animate-go  thickbox\">\n" +
+						spacing+"\t<a href=\""+image_folder+file+"\" class=\"b-link-stripe b-animate-go  thickbox\">\n" +
 							spacing+"\t\t<figure class=\"effect-bubba\">\n" +
-								spacing+"\t\t\t<img class=\"img-responsive\" src=\"images/miscellaneous/"+file+"\" alt=\"\">\n" +
+								spacing+"\t\t\t<img class=\"img-responsive\" src=\""+image_folder+file+"\" alt=\"\">\n" +
 							spacing+"\t\t</figure>\n" +
 						spacing+"\t</a>\n" +
 					spacing+"</div>\n")
@@ -28,3 +29,6 @@ contents = contents[:contents.find(insertion_start)+len(insertion_start)] + gall
 f = open(page_to_change, "w")
 f.write(contents)
 f.close()
+
+
+#%%
